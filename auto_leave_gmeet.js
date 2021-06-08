da04b3e52ae10055b181f243f236dd98f43c3ff8
Ifen Faridian <ifeniprun16@gmail.com>
2021-06-08 10:43:12 +0700

    update for new view google meet

--- auto_leave_gmeet.js
+++ auto_leave_gmeet.js
@@ -1,20 +1,18 @@
-var jumlah_orang_minimal = 3;//ketika jumlah orang kurang dari atau sama dengan ini maka otomatis akan keluar meet
+var jumlah_orang_minimal = 7;//ketika jumlah orang kurang dari atau sama dengan ini maka otomatis akan keluar meet
 
 console.log('MULAII!!');
 function logout(){
-    var reject = document.querySelectorAll('.VTBa7b , .MbhUzd')[12];
+    var reject = document.querySelectorAll('.yHy1rc , .eT1oJ , .tWDL4c , .jh0Tpd , .Gt6sbf , .QQrMi , .ftJPW')[4];
     reject.click();
     console.log('keluar');
     setTimeout(() => {
-//         var rejoin = document.querySelectorAll('.e19J0b , .CeoRYc')[0];
-//         rejoin.click();
 
     window.location.href = "https://meet.google.com/";
         
     }, 3000);
 }
 function cekOrang(){
-    var coba = document.querySelectorAll(".wnPUne , .N0PJ8e")[0].innerHTML;
+    var coba = document.querySelectorAll(".uGOf1d")[0].innerHTML;
     var jumlah = parseInt(coba);
     console.log('jumlah orang sekarang: '+jumlah);
     if(jumlah <= jumlah_orang_minimal){
